1a8963b869f2734ca93d573d0a94c5bc28126461
Paulo dos Santos <paulo@motovalor.com>
2021-08-23 20:24:40 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript'
+  ],
+  ignorePatterns: ['**/dist'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parserOptions: {
+        project: ['./tsconfig.json', '.packages/vite-plugin-leainx/tsconfig.json'] // Specify it only for TypeScript files
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+  },
+  globals: {
+    NodeJS: true
+  }
+}
