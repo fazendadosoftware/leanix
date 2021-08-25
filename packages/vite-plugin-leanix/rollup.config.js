06932976d949d494f431906e782b5a3626128408
Paulo dos Santos <paulo@motovalor.com>
2021-08-25 15:09:32 -0300

    current state

---
+++ packages/vite-plugin-leanix/rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: 'lib/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+    sourcemap: true,
+    exports: 'auto'
+  },
+  plugins: [
+    typescript({ tsconfig: './tsconfig.json', include: ['lib/**/*'], outputToFilesystem: true }),
+    json(),
+    nodeResolve({ preferBuiltins: true }),
+    commonjs(),
+    terser()
+  ]
+}
