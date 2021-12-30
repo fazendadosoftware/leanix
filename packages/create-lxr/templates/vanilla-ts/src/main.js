4f82a67fe1ad5c47c7dafda195dde93ffd829e15
Paulo dos Santos <pauloramires@gmail.com>
2021-12-30 14:09:33 -0300

    updated dependencies. bumped create-lxr to v0.0.27 (#5)
    Co-authored-by: Paulo <paulo@fazendadosoftware.com>

--- packages/create-lxr/templates/vanilla-ts/src/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./style.css");
-const app = document.querySelector('#app');
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
-//# sourceMappingURL=main.js.map
