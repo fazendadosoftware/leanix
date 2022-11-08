27c343956d134a68748bf53910c09051c62bb58e
Paulo <paulo@fazendadosoftware.com>
2022-11-07 21:39:00 -0300

    current state

---
+++ packages/lxr-core/build.ts
@@ -0,0 +1,25 @@
+import { build, BuildOptions, Format } from 'esbuild'
+
+const getOptions = (format: Format, fileExtension: string): BuildOptions => ({
+  entryPoints: ['lib/index.ts'],
+  outfile: `dist/index.${fileExtension}`,
+  bundle: true,
+  format,
+  sourcemap: true,
+  minify: true,
+  loader: { '.ts': 'ts' },
+  platform: 'node'
+})
+
+void (async () => {
+  const formats: Partial<Record<Format, string>> = {
+    esm: 'mjs',
+    cjs: 'cjs'
+  }
+
+  for (const [format, fileExtension] of Object.entries(formats)) {
+    const options = getOptions(format as Format, fileExtension)
+    await build(options)
+  }
+  console.log('⚡ Done')
+})()
