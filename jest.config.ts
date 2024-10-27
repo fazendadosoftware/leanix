bee304198946eae24270044732320a391ec0b5cb
Paulo Santos <paulo.santos04@sap.com>
2024-10-27 19:16:40 -0300

    WIP

--- jest.config.ts
+++ jest.config.ts
@@ -3,15 +3,6 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   verbose: true,
   testEnvironment: 'node',
-  projects: [
-    {
-      testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-      displayName: '@lxr/core',
-      testMatch: ['<rootDir>/packages/core/src/**/*.spec.ts'],
-      // TODO: // line 12 => https://stackoverflow.com/questions/69383514/node-fetch-3-0-0-and-jest-gives-syntaxerror-cannot-use-import-statement-outside
-      transformIgnorePatterns: []
-    }
-  ],
   transform: {
     '^.+\\.m?[tj]sx?$': [
       'babel-jest',
@@ -20,17 +11,19 @@ const config: Config.InitialOptions = {
       }
     ]
   },
-  coverageReporters: ['text', 'cobertura'],
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        suiteName: 'leanix-torg',
-        outputDirectory: 'test_results',
-        outputName: 'junit.xml'
-      }
-    ]
+  projects: [
+    {
+      displayName: '@lxr/core',
+      testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+      testMatch: ['<rootDir>/packages/core/src/**/*.spec.ts'],
+      // TODO: // line 12 => https://stackoverflow.com/questions/69383514/node-fetch-3-0-0-and-jest-gives-syntaxerror-cannot-use-import-statement-outside
+      transformIgnorePatterns: []
+    },
+    {
+      displayName: 'vite-plugin-lxr',
+      testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+      testMatch: ['<rootDir>/packages/vite-plugin-lxr/src/**/*.spec.ts']
+    }
   ]
 }
 export default config
